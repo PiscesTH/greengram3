4bc1b5536e18104e5bf04a1acf9ff839a3642a54
TH <kareme007@gmail.com>
2023-11-29 11:44:27 +0900

    23.11.19. greengram ver.3

---
+++ src/main/java/com/green/greengram3/user/UserController.java
@@ -0,0 +1,13 @@
+package com.green.greengram3.user;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping
+public class UserController {
+    private UserService service;
+}
