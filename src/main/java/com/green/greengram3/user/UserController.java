f805340575dcd7abfec9b069920a83377f967818
TH <kareme007@gmail.com>
2023-11-29 13:19:18 +0900

    23.11.19. greengram ver.3

--- src/main/java/com/green/greengram3/user/UserController.java
+++ src/main/java/com/green/greengram3/user/UserController.java
@@ -1,5 +1,10 @@
 package com.green.greengram3.user;
 
+import com.green.greengram3.common.ResVo;
+import com.green.greengram3.user.model.UserSignupDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -7,7 +12,13 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping
+@RequestMapping("/api/user")
 public class UserController {
-    private UserService service;
+    private final UserService service;
+
+    @Operation(summary = "회원가입", description = "회원가입용 정보")
+    @PostMapping("/signup")
+    public ResVo postSignup(@RequestBody UserSignupDto dto){
+        return service.signup(dto);
+    }
 }
--- src/main/java/com/green/greengram3/user/UserService.java
+++ src/main/java/com/green/greengram3/user/UserService.java
@@ -1,12 +1,28 @@
 package com.green.greengram3.user;
 
+import com.green.greengram3.common.ResVo;
+import com.green.greengram3.user.model.UserSignupDto;
+import com.green.greengram3.user.model.UserSignupProcDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
+import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.stereotype.*;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserService {
-    private UserMapper userMapper;
+    private final UserMapper userMapper;
+
+    public ResVo signup(UserSignupDto dto){
+        String hasedUpw = BCrypt.hashpw(dto.getUpw(),BCrypt.gensalt());
+        UserSignupProcDto pDto = UserSignupProcDto.builder()
+                .uid(dto.getUid())
+                .nm(dto.getNm())
+                .upw(hasedUpw)
+                .pic(dto.getPic())
+                .build();
+        int insResult = userMapper.insUser(pDto);
+        return new ResVo(pDto.getIuser());    //회원가입한 iuser pk 값 리턴
+    }
 }
